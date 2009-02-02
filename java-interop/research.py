938bae4b9935648f7b5a49197ade6917752d31aa
Konrad Delong <konryd@gmail.com>
2009-02-02 15:18:19 +0100

    tidying up

--- java-interop/research.py
+++
@@ -1,4 +0,0 @@
-from java.lang import ClassLoader
-
-cl = ClassLoader.getSystemClassLoader().getSystemResource("research.py")
-print cl
--- java-interop/run.sh
+++
@@ -1,8 +0,0 @@
-#CLASSPATH=$CLASSPATH:./egothor.jar:./detectors.jar:./morfologik-stemming.jar jython test.py
-CLASSPATH=$CLASSPATH:detectors.jar:jython.jar:egothor.jar:morfologik-stemming.jar javac jyinterface/factory/JythonFactory.java
-CLASSPATH=$CLASSPATH:detectors.jar:jython.jar:egothor.jar:morfologik-stemming.jar javac Main.java
-CLASSPATH=$CLASSPATH:detectors.jar:jython.jar:egothor.jar:morfologik-stemming.jar java Main
-#CLASSPATH=$CLASSPATH:./egothor.jar:./detectors.jar:./morfologik-stemming.jar jython research.py
-
-#CLASSPATH=$CLASSPATH:./egothor.jar:./detectors.jar:./morfologik-stemming.jar javac javasucks/Main.java 
-#CLASSPATH=$CLASSPATH:./egothor.jar:./detectors.jar:./morfologik-stemming.jar java javasucks.Main
